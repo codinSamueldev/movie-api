b34f0524c7f8904fab0a203ff4d36bc1429c5c5e
Samuel Henao <codinsamueldev@gmail.com>
2023-12-07 11:43:15 -0500

    feat: validate token function

--- jwt_manager.py
+++ jwt_manager.py
@@ -1,6 +1,12 @@
-from jwt import encode
+from jwt import encode, decode
 
 def create_token(data: dict):
     # Payload = content to transform into a token.
     token: str = encode(payload=data, key="password secured", algorithm="HS256")
     return token
+
+
+def validate_token(token: str) -> dict:
+    data: str = decode(token, key="password secured", algorithms=["HS256"])
+    return data
+

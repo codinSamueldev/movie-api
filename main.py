07829e07a7388cc0788564e28d66ae9e67bb15e0
Samuel Henao <codinsamueldev@gmail.com>
2024-02-01 17:08:56 -0500

    code refactor Remove unnecesary code such as modules, dependencies and schemas that were not used in the API

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 from routers import movies, jwt_manager_auth, html_endpoints, users
-from fastapi import FastAPI, Body, Path, Query, HTTPException, Depends
-from fastapi.responses import HTMLResponse, JSONResponse
-from pydantic import BaseModel, Field
-from starlette import status
-from typing import Any, Optional, List, Annotated
-
-from fastapi.encoders import jsonable_encoder
-from config.database import SessionLocal, engine, Base
-from models.movie import Movie as MovieModel
-from routers.jwt_manager_auth import oauth2_bearer, get_current_user
+from fastapi import FastAPI
+from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
-
 
 
 app = FastAPI()
@@ -27,20 +18,6 @@ app.version = "0.0.0.1"
 app.add_middleware(ErrorHandler)
 
 Base.metadata.create_all(bind=engine)
-
-
-class User(BaseModel):
-    email: str
-    password: str
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "email": "johndoe@example.com",
-                "password": "*********"
-            }
-        }
-
 
 #Tags=[] help us to separate each endpoint in the documentation so we can see, verify and test each endpoint.
 @app.get('/', tags=["Casita"])
